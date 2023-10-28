5b551100e58ddd1e0559fb21a5967c937be16f60
zahoorshinwari <khanzshinwari5371@gmail.com>
2023-10-28 12:27:37 +0500

    changing the name state using the dispatch and use selector

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeName } from '../store'
+
 
 function CarForm() {
+
+  const dispatch = useDispatch()
+
+  // accessing the state
+  const name = useSelector((state) => {
+    return state.form.name
+  })
+
+  // changing the state using dispatch function which is changename
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value))
+  }
+
   return (
-    <div>CarForm</div>
+    <div className='car-form panel'>
+      <h4 className='subtitle is-3'>Add Car</h4>
+      <form>
+        <div className='field-group'>
+          <div className='field'>
+            <label className='label'>Name:</label>
+            <input 
+              className='input is-expanded'
+              value={name}
+              onChange={handleNameChange }
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
