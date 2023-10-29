50a05496c063def0cd612b99198745ecb7395fbe
zahoorshinwari <khanzshinwari5371@gmail.com>
2023-10-29 22:51:26 +0500

    adding some style using bulma css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import 'bulma/css/bulma.css'
+import './styles.css'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
